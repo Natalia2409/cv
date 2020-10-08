6f7b004caf8ae04a2599a879f2717dd871996c7c
Natalia <nataliarudomyr@gmail.com>
2020-10-08 10:47:13 +0300

    Updates

---
+++ precache-manifest.7329496299df9bccfa60c5c11fa5487f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8490186a050239b7852b507b2a4ae8f",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "2517ded57fa033cc796c",
+    "url": "/cv/static/css/main.a05ea4fe.chunk.css"
+  },
+  {
+    "revision": "3cd1e0637602bd95eb89",
+    "url": "/cv/static/js/2.42f0d8ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv/static/js/2.42f0d8ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2517ded57fa033cc796c",
+    "url": "/cv/static/js/main.b0da4f91.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "f1bc25cf941a513c66356861f5d25f09",
+    "url": "/cv/static/media/photo.f1bc25cf.JPG"
+  }
+]);
